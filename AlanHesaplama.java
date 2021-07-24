1f076073f8ee126ee0411c2a145619ade0a5f715
gokhangemici <gokhan_gemici@hotmail.com>
2021-07-24 22:55:40 +0300

    ucgenin alanini hesaplayan program
    Ödev
    Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
    
    Formül
    Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
    
    𝑢 = (a+b+c) / 2
    
    Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)

--- AlanHesaplama.java
+++ AlanHesaplama.java
@@ -1,26 +1,24 @@
 package Giris;
 
-
+import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 
 public class AlanHesaplama {
-    public static void main(String[] args) {
 
-        int a, b, c, cevre, u;
-        double  alan;
+    public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
+        int a, b, c, cevre, u, alan;
+
         System.out.print("1.kenari gir: ");
         a = inp.nextInt();
-        System.out.print("2.kenarı gir: ");
+        System.out.print("2.kenari gir: ");
         b = inp.nextInt();
         System.out.print("3.kenari gir: ");
         c = inp.nextInt();
 
         cevre = a + b + c;
         u = cevre / 2;
-        alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
-        System.out.println("Ucgenin alani: " + alan);
-
-
+        alan = u * (u - a) * (u - b) * (u - c);
+        System.out.println("Alan: " + alan);
     }
 }
