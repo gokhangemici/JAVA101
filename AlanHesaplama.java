96d54c36927095381d5f48a5c3ea3c5f807c8b6a
gokhangemici <gokhan_gemici@hotmail.com>
2021-07-25 14:39:18 +0300

    Update AlanHesaplama.java

--- AlanHesaplama.java
+++ AlanHesaplama.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class AlanHesaplama {
 
     public static void main(String[] args) {
-
         Scanner inp = new Scanner(System.in);
+        
+        int a, b, c, u, cevre;
+        double alan;
+        System.out.println("1.kenari gir: ");
+        a = inp.nextInt();
+        System.out.println("2.kenari gir: ");
+        b = inp.nextInt();
+        System.out.println("3.kenari gir: ");
+        c = inp.nextInt();
+        
+        cevre = a + b + c;
+        u = cevre / 2;
+        alan = Math.sqrt((u * (u - a) * (u - b) * (u - c)));
+        System.out.println("Ucgenin alani: " + alan);
 
     }
 }
