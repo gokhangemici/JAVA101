aa992a536f71e607b2159b745e91c51d415e2fc3
gokhangemici <gokhan_gemici@hotmail.com>
2021-07-29 21:40:29 +0300

    üç sayıyı küçükten büyüğe doğru sıralayan program

---
+++ SayiSiralama.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SayiSiralama {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("1.sayi: ");
+        a = inp.nextInt();
+        System.out.print("2.sayi: ");
+        b = inp.nextInt();
+        System.out.print("3.sayi: ");
+        c = inp.nextInt();
+
+        if((a < b) && (a < c))
+        {
+            if(b < c)
+            {
+                System.out.println("a < b < c");
+            }
+            else
+            {
+                System.out.println("a < c < b");
+            }
+        }
+        else if((b < a) && (b < c))
+        {
+            if(a < c)
+            {
+                System.out.println("b < a < c");
+            }
+            else
+            {
+                System.out.println("b < c < a");
+            }
+        }
+        else
+        {
+            if(b < a)
+            {
+                System.out.println("c < b < a");
+            }
+            else
+            {
+                System.out.println("c < a < b");
+            }
+        }
+
+
+    }
+}
